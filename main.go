274e4b871d13b70340997f656915094426b25a62
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-27 16:45:36 +0300

    create canvas

--- main.go
+++ main.go
@@ -3,34 +3,19 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
-	"time"
+	"fyne.io/fyne/v2/canvas"
+	"image/color"
 )
-
-func updateTime(clock *widget.Label) {
-	formatted := time.Now().Format("Time: 15:04:05")
-	clock.SetText(formatted)
-}
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Clock")
-	w.Resize(fyne.NewSize(1280, 720))
-	clock := widget.NewLabel("")
-	updateTime(clock)
-	w.SetContent(clock)
+	w := a.NewWindow("Canvas")
+	myCanvas := w.Canvas()
 
-	w2 := a.NewWindow("Second")
-	w2.Resize(fyne.NewSize(450, 300))
-	w2.SetContent(clock)
+	blue := color.NRGBA{B: 255, A: 255}
+	rect := canvas.NewRectangle(blue)
+	myCanvas.SetContent(rect)
 
-	go func() {
-		for range time.Tick(time.Second) {
-			updateTime(clock)
-		}
-	}()
-
-	w.Show()
-	w2.Show()
-	a.Run()
+	w.Resize(fyne.NewSize(300, 300))
+	w.ShowAndRun()
 }
