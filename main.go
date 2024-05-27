aca6b4396206ea3e3cd7321e6e88f4d2130386d7
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-27 16:53:50 +0300

    add text or circle

--- main.go
+++ main.go
@@ -7,14 +7,32 @@ import (
 	"image/color"
 )
 
+func setContentToText(c fyne.Canvas) {
+	green := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
+	text := canvas.NewText("Text", green)
+	text.TextStyle.Bold = true
+	c.SetContent(text)
+}
+
+func setContentToCircle(c fyne.Canvas) {
+	red := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	circle := canvas.NewCircle(color.White)
+	circle.StrokeWidth = 4
+	circle.StrokeColor = red
+	c.SetContent(circle)
+}
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Canvas")
 	myCanvas := w.Canvas()
 
-	blue := color.NRGBA{B: 255, A: 255}
-	rect := canvas.NewRectangle(blue)
-	myCanvas.SetContent(rect)
+	//blue := color.NRGBA{B: 255, A: 255}
+	//rect := canvas.NewRectangle(blue)
+	//myCanvas.SetContent(rect)
+
+	//setContentToText(myCanvas)
+	setContentToCircle(myCanvas)
 
 	w.Resize(fyne.NewSize(300, 300))
 	w.ShowAndRun()
