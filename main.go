67bcecbf73f26e501f1546a9ac0caf61ebad68d1
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-29 17:17:00 +0300

    add raster from image

--- main.go
+++ main.go
@@ -4,18 +4,30 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"image/color"
-	"math/rand"
+	"image/jpeg"
+	"log"
+	"os"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	raster := canvas.NewRasterWithPixels(func(_, _, w, h int) color.Color {
-		return color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 0xff}
-	})
+	//raster := canvas.NewRasterWithPixels(func(_, _, w, h int) color.Color {
+	//	return color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 0xff}
+	//})
 
+	catFile, err := os.Open("url")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, err := jpeg.Decode(catFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	raster := canvas.NewRasterFromImage(img)
 	w.SetContent(raster)
 	w.Resize(fyne.NewSize(1280, 720))
 
