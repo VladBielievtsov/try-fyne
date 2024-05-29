42c0418d680ebafdcd2e20dfdfd90ab884b5973d
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-29 17:26:05 +0300

    add animation

--- main.go
+++ main.go
@@ -4,17 +4,31 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"image/color"
+	"time"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	gradient := canvas.NewHorizontalGradient(color.White, color.Transparent)
+	obj := canvas.NewRectangle(color.Black)
+	obj.Resize(fyne.NewSize(50, 50))
+	w.SetContent(container.NewWithoutLayout(obj))
 
-	w.SetContent(gradient)
-	w.Resize(fyne.NewSize(1280, 720))
+	red := color.NRGBA{R: 0xff, A: 0xff}
+	blue := color.NRGBA{B: 0xff, A: 0xff}
+	canvas.NewColorRGBAAnimation(red, blue, time.Second*2, func(c color.Color) {
+		obj.FillColor = c
+		canvas.Refresh(obj)
+	}).Start()
 
+	move := canvas.NewPositionAnimation(fyne.NewPos(0, 0), fyne.NewPos(200, 0), time.Second, obj.Move)
+	move.AutoReverse = true
+	move.Start()
+
+	w.Resize(fyne.NewSize(250, 50))
+	w.SetPadded(false)
 	w.ShowAndRun()
 }
