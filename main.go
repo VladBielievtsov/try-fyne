5379bdd8cc75d309259a030a2ca3a0d166420a8e
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-29 17:19:39 +0300

    add gradient

--- main.go
+++ main.go
@@ -4,31 +4,16 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"image/jpeg"
-	"log"
-	"os"
+	"image/color"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	//raster := canvas.NewRasterWithPixels(func(_, _, w, h int) color.Color {
-	//	return color.RGBA{R: uint8(rand.Intn(255)), G: uint8(rand.Intn(255)), B: uint8(rand.Intn(255)), A: 0xff}
-	//})
+	gradient := canvas.NewHorizontalGradient(color.White, color.Transparent)
 
-	catFile, err := os.Open("url")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	img, err := jpeg.Decode(catFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	raster := canvas.NewRasterFromImage(img)
-	w.SetContent(raster)
+	w.SetContent(gradient)
 	w.Resize(fyne.NewSize(1280, 720))
 
 	w.ShowAndRun()
