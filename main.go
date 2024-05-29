f0de8bc4fd25e5cee08088dce847121d91f20b26
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-29 16:41:41 +0300

    add image

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-	"image/color"
+	"fyne.io/fyne/v2/storage"
+	"log"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	circle := canvas.NewCircle(color.White)
-	circle.StrokeColor = color.Gray{Y: 0x99}
-	w.SetContent(circle)
+	url := "https://avatars.githubusercontent.com/u/40768241?v=4"
+
+	uri, err := storage.ParseURI(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	image := canvas.NewImageFromURI(uri)
+
+	image.FillMode = canvas.ImageFillOriginal
+	w.SetContent(image)
 	w.Resize(fyne.NewSize(1280, 720))
 
 	w.ShowAndRun()
