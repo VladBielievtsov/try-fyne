d930c85fa66205c35c7b4e6df68d8a1be3ee7df8
VladBielievtsov <vladbielievtsov@gmail.com>
2024-05-30 14:05:19 +0300

    widgets: label, button, entry

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"log"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter text...")
+
 	tabs := container.NewAppTabs(
 		container.NewTabItem("Tab 1", widget.NewLabel("Hello")),
-		container.NewTabItem("Tab 2", widget.NewLabel("There")),
+		container.NewTabItem("Tab 2", container.NewVBox(input, widget.NewButton("Save", func() {
+			log.Println("Content was: ", input.Text)
+		}))),
 	)
 
 	tabs.SetTabLocation(container.TabLocationLeading)
